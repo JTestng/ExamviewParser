2d6c918d0a0c404a8a1b3b5987e48bfb83645654
Arjun Vikram <arjvik@gmail.com>
2018-11-22 20:36:56 -0600

    stuff

--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,6 @@
 module com.arjvik.jtesting.examviewparser {
 	requires static lombok;
 	requires java.xml;
+	exports com.arjvik.jtesting.examviewparser;
+	exports com.arjvik.jtesting.examviewparser.domain;
 }
